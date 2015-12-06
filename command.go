897be414cd30e9f9e0f8a05dd29b4554b4a7c324
suonlight <minh.nh1989@gmail.com>
2015-12-06 10:28:38 +0700

    add list

--- command.go
+++ command.go
@@ -1,4 +1,4 @@
-import main 
+package main
 
 type Store map[string]interface{}
 
@@ -10,28 +10,4 @@ func (s *Store) StringGet(key string) interface {
 	return s[key]
 }
 
-func (s *Store) ListPush(key string, value []string) {
-	if len(s[key]) == 0 {
-		s[key] = value
-	} else {
-		s[key] = append(s[key].([]string), value)
-	} 
-}
 
-func (s *Store) ListRightPop(key string) interface {
-	if len(s[key]) == 0 {
-		return interface{}
-	} else {
-		v := s[key]
-		return v[0]
-	}
-}
-
-
-func (s *Store) ListRange(key string, start int, stop int) map[string]interface {
-	if (start < 0 || stop < 0) {
-		return 
-	}
-
-
-}
---
+++ list.go
@@ -0,0 +1,59 @@
+package main
+
+import "errors"
+
+const OK = "OK"
+
+func (s *Store) LLEN(key string) int {
+	return len(s[key].([]interface{}))
+}
+
+func (s *Store) RPUSH(key string, value ...[]string) string {
+	if _, ok := s[key]; !ok {
+		s[key] = value
+	} else {
+		s[key] = append(s[key].([]string), value...)
+	}
+	return OK
+}
+
+func (s *Store) LPOP(key string) string {
+	if _, ok := s[key]; !ok {
+		return ""
+	}
+	a := s[key].([]string)
+	s[key] = a[1:]
+
+	return a[0]
+}
+
+func (s *Store) RPOP(key string) string {
+	if _, ok := s[key]; !ok {
+		return ""
+	}
+
+	a := s[key].([]string)
+	if len(a) > 1 {
+		s[key] = a[:len(a)-1]
+	} else {
+		delete(s, key)
+	}
+	return a[len(a)-1]
+}
+
+func (s *Store) LRANGE(key string, start int, stop int) ([]string, error) {
+	if start < 0 || stop < 0 {
+		return nil, errors.New("EINV")
+	}
+
+	if _, ok := s[key]; !ok {
+		return []string{}, nil
+	}
+
+	a := s[key].([]string)
+	if len(a) > stop {
+		return a[start:stop], nil
+	}
+
+	return nil, errors.New("EINV")
+}
