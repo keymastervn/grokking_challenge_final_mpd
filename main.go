a0492763f53f4a9a5bae5046400ecc93709149bb
suonlight <minh.nh1989@gmail.com>
2015-12-06 10:07:34 +0700

    main

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Command struct {
+	Cmd string `json:"command"`
+}
 
 func main() {
 	r := gin.Default()
@@ -9,7 +18,43 @@ func main() {
 }
 
 func ledis(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"command": "OK",
-	})
+	// validate content-type & content-length
+	var command Command
+	if c.BindJSON(&command) == nil {
+		result, err := Execute(command.Cmd)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"response": err.Error(),
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"response": result,
+			})
+		}
+	} else {
+		c.JSON(200, gin.H{
+			"response": "ECOM",
+		})
+	}
+}
+
+func Execute(cmd string) (store map[string]interface{}, err error) {
+	args := strings.Split(cmd, " ")
+	if len(args) == 0 {
+		err = errors.New("ECOM")
+		return
+	}
+
+	c := strings.ToUpper(args[0])
+	k1 := args[1]
+	if k1 != strings.ToLower(k1) {
+		err = errors.New("ECOM")
+		return
+	}
+
+	if c == "SET" {
+
+	}
+
+	return
 }
